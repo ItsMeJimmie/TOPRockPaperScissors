8ec6dd05f8914fdb119c4252132abea52cc64842
Jimmie Cox <jamescox995@gmail.com>
2024-06-06 21:34:06 -0400

    Here is my computer and human choice functions

--- rps.js
+++ rps.js
@@ -1,1 +1,23 @@
-console.log("Hellow World");
+function getComputerChoice (min, max) {
+    const random = Math.random();
+    const number = Math.floor(random * 3) + 1;
+
+    switch (number) {
+        case 1 :
+            return "rock";
+        case 2 :
+            return "paper";
+        case 3 :
+            return "scissors";
+        default :
+            return "try again";
+    }
+} 
+
+function getHumanChoice() {
+
+    let userChoice = prompt("Rock, Paper, Scissors?: ");
+
+    return userChoice;
+}
+
