a1d3cf7e886e7a08963e66be79700277fe14b0fd
Jimmie Cox <jamescox995@gmail.com>
2024-06-06 21:54:27 -0400

    Filled in the rest

--- rps.js
+++ rps.js
@@ -17,7 +17,33 @@ function getComputerChoice (min, max) {
 function getHumanChoice() {
 
     let userChoice = prompt("Rock, Paper, Scissors?: ");
+    userChoice.toLowerCase();
 
     return userChoice;
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+
+    if(humanChoice === computerChoice){
+        console.log("Tie!!");
+    } else if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper") ) {
+        humanScore ++;
+        console.log(`Human wins!! ${humanChoice} beats ${computerChoice}!!`);
+    } else {
+        computerScore ++;
+        console.log(`Computer wins!! ${computerChoice} beats ${humanChoice}!!`);
+    }
+}
+
+function playGame() {
+    for(let i = 1; i <= 5; i++) {
+        playRound(getHumanChoice(),getComputerChoice(1,3))
+        console.log(`Player Score: ${humanScore} and computer score: ${computerScore}`);
+    }
+    
+}
+
+console.log(playGame());
